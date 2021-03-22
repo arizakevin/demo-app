e01a865216192954804cced5d183a4140265e110
kevwas <arizakevin.dev@example.com>
2021-03-21 22:27:16 -0500

    Updating files

--- Intro.js
+++ Intro.js
@@ -8,7 +8,7 @@ class Intro extends Component {
             <View style={styles.container}>
                 <Text style={styles.welcome}>Welcome to React Native!</Text>
                 <Text style={styles.instructions}>
-                This is a React Native snapshot test.
+                This is a React Native snapshot test. :)
                 </Text>
             </View>
         );
